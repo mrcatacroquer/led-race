e7a5b026f91de71656b26ad2c2d177344377ca77
mrcatacroquer <manulucio@gmail.com>
2019-06-09 15:35:48 +0000

    Flow refactor
    Created smaller funtions.
    Removed magic numbers.
    Used (IMHO) more descriptive variables.

--- led_race3_5_AD19_SMS.ino
+++ led_race3_5_AD19_SMS.ino
@@ -38,6 +38,8 @@ int NPIXELS=MAXLED; // leds on track
 #define COLOR1    track.Color(255,0,0)
 #define COLOR2    track.Color(0,255,0)
 
+#define DEF_GRAVITY 127
+
 int win_music[] = {
   2637, 2637, 0, 2637, 
   0, 2093, 2637, 0,
@@ -53,12 +55,11 @@ float speed2=0;
 float dist1=0;
 float dist2=0;
 
-byte loop1=0;
-byte loop2=0;
+byte laps_player1=0;
+byte laps_player2=0;
 
 byte leader=0;
-byte loop_max=5; //total laps race
-
+byte laps_max=5; //total laps race
 
 float ACEL=0.2;
 float kf=0.015; //friction constant
@@ -72,75 +73,315 @@ unsigned long timestamp=0;
 
 Adafruit_NeoPixel track = Adafruit_NeoPixel(MAXLED, PIN_LED, NEO_GRB + NEO_KHZ800);
 
-int tdelay = 5; 
-
-void set_ramp(byte H,byte a,byte b,byte c)
-{for(int i=0;i<(b-a);i++){gravity_map[a+i]=127-i*((float)H/(b-a));};
- gravity_map[b]=127; 
- for(int i=0;i<(c-b);i++){gravity_map[b+i+1]=127+H-i*((float)H/(c-b));};
-}
-
-void set_loop(byte H,byte a,byte b,byte c)
-{for(int i=0;i<(b-a);i++){gravity_map[a+i]=127-i*((float)H/(b-a));};
- gravity_map[b]=255; 
- for(int i=0;i<(c-b);i++){gravity_map[b+i+1]=127+H-i*((float)H/(c-b));};
-}
-
+int loop_delay_time = 5; 
 
 void setup() {
-  for(int i=0;i<NPIXELS;i++){gravity_map[i]=127;};
-  track.begin(); 
+  for(int i=0;i<NPIXELS;i++)
+  {
+    gravity_map[i]= DEF_GRAVITY;
+  };
+  
+  track.begin();
+  
   pinMode(PIN_P1,INPUT_PULLUP); 
   pinMode(PIN_P2,INPUT_PULLUP);  
 
   if ((digitalRead(PIN_P1)==0)) //push switch 1 on reset for activate physic
   {
     set_ramp(12,90,100,110);    // ramp centred in LED 100 with 10 led fordward and 10 backguard 
-    for(int i=0;i<NPIXELS;i++){track.setPixelColor(i, track.Color(0,0,(127-gravity_map[i])/8) );};
+    
+    for(int i=0;i<NPIXELS;i++)
+    {
+      track.setPixelColor(i, track.Color(0,0,(DEF_GRAVITY - gravity_map[i])/8) );
+    };
+    
     track.show();
   };
+  
   start_race();    
 }
 
-void start_race(){for(int i=0;i<NPIXELS;i++){track.setPixelColor(i, track.Color(0,0,0));};
-                  track.show();
-                  delay(2000);
-                  track.setPixelColor(12, track.Color(0,255,0));
-                  track.setPixelColor(11, track.Color(0,255,0));
-                  track.show();
-                  tone(PIN_AUDIO,400);
-                  delay(2000);
-                  noTone(PIN_AUDIO);                  
-                  track.setPixelColor(12, track.Color(0,0,0));
-                  track.setPixelColor(11, track.Color(0,0,0));
-                  track.setPixelColor(10, track.Color(255,255,0));
-                  track.setPixelColor(9, track.Color(255,255,0));
-                  track.show();
-                  tone(PIN_AUDIO,600);
-                  delay(2000);
-                  noTone(PIN_AUDIO);                  
-                  track.setPixelColor(9, track.Color(0,0,0));
-                  track.setPixelColor(10, track.Color(0,0,0));
-                  track.setPixelColor(8, track.Color(255,0,0));
-                  track.setPixelColor(7, track.Color(255,0,0));
-                  track.show();
-                  tone(PIN_AUDIO,1200);
-                  delay(2000);
-                  noTone(PIN_AUDIO);                               
-                  timestamp=0;              
-                 };
+void loop() {
+  set_track_base_color();
+    
+  calculate_player1_position_delta();
 
-void winner_fx() {
-               int msize = sizeof(win_music) / sizeof(int);
-               for (int note = 0; note < msize; note++) {
-               tone(PIN_AUDIO, win_music[note],200);
-               delay(230);
-               noTone(PIN_AUDIO);
+  calculate_player2_position_delta();
 
+  update_players_position();
+
+  update_leader();
+      
+  check_players_new_lap();
+
+  check_for_finished_race();
+    
+  draw_cars();
+  
+  delay(loop_delay_time);
+    
+  update_tbeep();  
 }
 
-                                               
-              };
+void update_players_position()
+{
+  dist1+=speed1;
+  dist2+=speed2;
+}
+
+void calculate_player1_position_delta()
+{
+  if ( (flag_sw1==1) && (digitalRead(PIN_P1)==0) ) 
+  {
+    flag_sw1=0;
+    speed1+=ACEL;
+  };
+    
+  if ( (flag_sw1==0) && (digitalRead(PIN_P1)==1) )
+  {
+    flag_sw1=1;
+  };
+
+  if ((gravity_map[(word)dist1 % NPIXELS])< DEF_GRAVITY)
+    speed1-=kg*(DEF_GRAVITY - (gravity_map[(word)dist1 % NPIXELS]));
+      
+  if ((gravity_map[(word)dist1 % NPIXELS])> DEF_GRAVITY)
+    speed1+=kg*((gravity_map[(word)dist1 % NPIXELS])- DEF_GRAVITY);    
+    
+  speed1-=speed1*kf;
+}
+
+void calculate_player2_position_delta()
+{
+  if ( (flag_sw2==1) && (digitalRead(PIN_P2)==0) ) 
+  {
+    flag_sw2=0;
+    speed2+=ACEL;
+  };
+    
+  if ( (flag_sw2==0) && (digitalRead(PIN_P2)==1) ) 
+  {
+    flag_sw2=1;
+  };
+
+  if ((gravity_map[(word)dist2 % NPIXELS])<DEF_GRAVITY)
+    speed2-=kg*(DEF_GRAVITY-(gravity_map[(word)dist2 % NPIXELS]));
+      
+  if ((gravity_map[(word)dist2 % NPIXELS])>DEF_GRAVITY)
+    speed2+=kg*((gravity_map[(word)dist2 % NPIXELS])-DEF_GRAVITY);
+        
+  speed2-=speed2*kf; 
+}
+
+void set_ramp(byte H,byte a,byte b,byte c)
+{
+  for(int i=0;i<(b-a);i++)
+  {
+    gravity_map[a+i]=DEF_GRAVITY -i*((float)H/(b-a));
+  };
+  
+  gravity_map[b]=DEF_GRAVITY;
+  
+  for(int i=0;i<(c-b);i++)
+  {
+    gravity_map[b+i+1]=DEF_GRAVITY +H-i*((float)H/(c-b));
+  };
+}
+
+void set_loop(byte H,byte a,byte b,byte c)
+{
+  for(int i=0;i<(b-a);i++)
+  {
+    gravity_map[a+i]=DEF_GRAVITY - i*((float)H/(b-a));
+  };
+  
+  gravity_map[b]=255;
+  
+  for(int i=0;i<(c-b);i++)
+  {
+    gravity_map[b+i+1]=DEF_GRAVITY + H-i*((float)H/(c-b));
+  };
+}
+
+void start_race(){
+  for(int i=0;i<NPIXELS;i++)
+  {
+    track.setPixelColor(i, track.Color(0,0,0));
+  };
+  
+  track.show();
+  delay(2000);
+  track.setPixelColor(12, track.Color(0,255,0));
+  track.setPixelColor(11, track.Color(0,255,0));
+  track.show();
+  tone(PIN_AUDIO,400);
+  delay(2000);
+  noTone(PIN_AUDIO);                  
+  track.setPixelColor(12, track.Color(0,0,0));
+  track.setPixelColor(11, track.Color(0,0,0));
+  track.setPixelColor(10, track.Color(255,255,0));
+  track.setPixelColor(9, track.Color(255,255,0));
+  track.show();
+  tone(PIN_AUDIO,600);
+  delay(2000);
+  noTone(PIN_AUDIO);                  
+  track.setPixelColor(9, track.Color(0,0,0));
+  track.setPixelColor(10, track.Color(0,0,0));
+  track.setPixelColor(8, track.Color(255,0,0));
+  track.setPixelColor(7, track.Color(255,0,0));
+  track.show();
+  tone(PIN_AUDIO,1200);
+  delay(2000);
+  noTone(PIN_AUDIO);     
+                            
+  timestamp=0;                 
+};
+
+void winner_fx()
+{
+  int msize = sizeof(win_music) / sizeof(int);
+   
+  for (int note = 0; note < msize; note++)
+  {
+    tone(PIN_AUDIO, win_music[note],200);
+    delay(230);
+    noTone(PIN_AUDIO);
+  }
+};
+
+void draw_car1(void){
+  for(int i=0;i<=laps_player1;i++)
+  {
+    track.setPixelColor(((word)dist1 % NPIXELS)+i, track.Color(0,255-i*20,0));
+  };                   
+}
+
+void draw_car2(void){
+  for(int i=0;i<=laps_player2;i++)
+  {
+    track.setPixelColor(((word)dist2 % NPIXELS)+i, track.Color(255-i*20,0,0));
+  };            
+}
+
+void set_track_base_color()
+{
+  for(int i=0;i<NPIXELS;i++)
+  {
+    track.setPixelColor(i, track.Color(0,0,(DEF_GRAVITY - gravity_map[i])/8) );
+  };
+}
+
+void update_leader()
+{
+  if (dist1>dist2)
+    leader=1;
+  else
+    leader=2;
+}
+
+void update_tbeep()
+{
+  if (TBEEP>0) 
+  {
+    TBEEP-=1;
+    if (TBEEP==0)
+    {
+      noTone(PIN_AUDIO);
+    }; // lib conflict !!!! interruption off by neopixel
+  }
+}
+
+void check_players_new_lap()
+{
+  if (dist1 > NPIXELS*laps_player1)
+  {
+    laps_player1++;
+    tone(PIN_AUDIO,600);
+    TBEEP=2;
+  }
+    
+  if (dist2 > NPIXELS*laps_player2)
+  {
+    laps_player2++;
+    tone(PIN_AUDIO,700);
+    TBEEP=2;
+  }
+}
+
+void check_for_finished_race()
+{
+  if (laps_player1>laps_max)
+  {
+    finish_game(1);
+  }
+    
+  if (laps_player2>laps_max)
+  {
+    finish_game(2);
+  }
+}
+
+void change_draworder()
+{
+  if ((millis() & 512)==(512 * draworder)) 
+  {
+    if (draworder==0) 
+    {
+      draworder=1;
+    }
+    else 
+    {
+      draworder=0;
+    }   
+  }
+}
+
+void draw_cars()
+{
+  change_draworder();
+  
+  if (draworder==0)
+  {
+    draw_car1();
+    draw_car2();
+  }
+  else 
+  {
+    draw_car2();
+    draw_car1();
+  }
+  
+  track.show();  
+}
+
+void finish_game(int player_who_won)
+{
+  uint32_t winner_color = track.Color(0,0,0);
+      
+  if (player_who_won == 1)
+    winner_color = COLOR1;
+  if (player_who_won == 2)
+    winner_color = COLOR2;
+        
+  for(int i=0;i<NPIXELS;i++)
+    track.setPixelColor(i, winner_color);
+  
+  track.show();
+  winner_fx();
+  reset_race_data();
+  start_race();
+}
+
+void reset_race_data()
+{
+  laps_player1=0;
+  laps_player2=0;
+  dist1=0;
+  dist2=0;
+  speed1=0;
+  speed2=0;
+  timestamp=0;
+}
 
 void burning1(){
 //to do
@@ -229,65 +470,4 @@ int display_lcd_laps(){
 
 int display_lcd_time(){
   //to do  
-}
-
-
-
-void draw_car1(void){for(int i=0;i<=loop1;i++){track.setPixelColor(((word)dist1 % NPIXELS)+i, track.Color(0,255-i*20,0));};                   
-  }
-
-void draw_car2(void){for(int i=0;i<=loop2;i++){track.setPixelColor(((word)dist2 % NPIXELS)+i, track.Color(255-i*20,0,0));};            
- }
-  
-void loop() {
-    //for(int i=0;i<NPIXELS;i++){track.setPixelColor(i, track.Color(0,0,0));};
-    for(int i=0;i<NPIXELS;i++){track.setPixelColor(i, track.Color(0,0,(127-gravity_map[i])/8) );};
-    
-    if ( (flag_sw1==1) && (digitalRead(PIN_P1)==0) ) {flag_sw1=0;speed1+=ACEL;};
-    if ( (flag_sw1==0) && (digitalRead(PIN_P1)==1) ) {flag_sw1=1;};
-
-    if ((gravity_map[(word)dist1 % NPIXELS])<127) speed1-=kg*(127-(gravity_map[(word)dist1 % NPIXELS]));
-    if ((gravity_map[(word)dist1 % NPIXELS])>127) speed1+=kg*((gravity_map[(word)dist1 % NPIXELS])-127);
-    
-    
-    speed1-=speed1*kf; 
-    
-    if ( (flag_sw2==1) && (digitalRead(PIN_P2)==0) ) {flag_sw2=0;speed2+=ACEL;};
-    if ( (flag_sw2==0) && (digitalRead(PIN_P2)==1) ) {flag_sw2=1;};
-
-    if ((gravity_map[(word)dist2 % NPIXELS])<127) speed2-=kg*(127-(gravity_map[(word)dist2 % NPIXELS]));
-    if ((gravity_map[(word)dist2 % NPIXELS])>127) speed2+=kg*((gravity_map[(word)dist2 % NPIXELS])-127);
-        
-    speed2-=speed2*kf; 
-        
-    dist1+=speed1;
-    dist2+=speed2;
-
-    if (dist1>dist2) {leader=1;} 
-    if (dist2>dist1) {leader=2;};
-      
-    if (dist1>NPIXELS*loop1) {loop1++;tone(PIN_AUDIO,600);TBEEP=2;};
-    if (dist2>NPIXELS*loop2) {loop2++;tone(PIN_AUDIO,700);TBEEP=2;};
-
-    if (loop1>loop_max) {for(int i=0;i<NPIXELS;i++){track.setPixelColor(i, track.Color(0,255,0));}; track.show();
-                                                    winner_fx();loop1=0;loop2=0;dist1=0;dist2=0;speed1=0;speed2=0;timestamp=0;
-                                                    start_race();
-                                                   }
-    if (loop2>loop_max) {for(int i=0;i<NPIXELS;i++){track.setPixelColor(i, track.Color(255,0,0));}; track.show();
-                                                    winner_fx();loop1=0;loop2=0;dist1=0;dist2=0;speed1=0;speed2=0;timestamp=0;
-                                                    start_race();
-                                                   }
-    if ((millis() & 512)==(512*draworder)) {if (draworder==0) {draworder=1;}
-                          else {draworder=0;}   
-                         }; 
-
-    if (draworder==0) {draw_car1();draw_car2();}
-        else {draw_car2();draw_car1();}   
-                 
-    track.show(); 
-    delay(tdelay);
-    
-    if (TBEEP>0) {TBEEP-=1; 
-                  if (TBEEP==0) {noTone(PIN_AUDIO);}; // lib conflict !!!! interruption off by neopixel
-                 };   
 }
